98aa6c67742946e0a38b0b159b284594f5870260
Zahraa Al-Hussaini <zhori1993@gmail.com>
2024-08-06 21:04:28 +0300

    task

---
+++ arrays2.js
@@ -0,0 +1,69 @@
+/**
+ * isArrayLengthOdd(numbers):
+ * - receives array `numbers`
+ * - returns true if array has an odd number of elements
+ * - returns false otherwise
+ *
+ * e.g.
+ * isArrayLengthOdd([1, 2, 3]) -> true
+ * isArrayLengthOdd([1, 2, 3, 4]) -> flase
+ */
+function isArrayLengthOdd(numbers) {
+  const length = numbers.length;
+
+  if (length % 2 == 0) {
+    return "false";
+  } else {
+    return "true";
+  }
+}
+
+console.log(isArrayLengthOdd([1, 2, 3]));
+
+/**
+ * isArrayLengthEven(numbers):
+ * - receives array `numbers`
+ * - returns true if array has an even number of elements
+ * - returns false otherwise
+ *
+ * e.g.
+ * isArrayLengthEven([1, 2, 3]) -> false
+ * isArrayLengthEven([1, 2, 3, 4]) -> true
+ */
+function isArrayLengthEven(numbers) {
+  const length = numbers.length;
+
+  if (length % 2 == 0) {
+    return "true";
+  } else {
+    return "false";
+  }
+}
+console.log(isArrayLengthEven([1, 2, 3, 4]));
+
+/**
+ * addLailaToArray(instructors):
+ * - receives array `instructors`
+ * - returns a new array that's a copy of array `instructors` with additional string "Laila"
+ *
+ * e.g.
+ * addLailaToArray(["Mshary", "Hasan"]) -> ["Mshary", "Hasan", "Laila"]
+ */
+function addLailaToArray(instructors) {
+  instructors.push("Laila");
+  return instructors;
+}
+console.log(addLailaToArray(["Mshary", "Hasan"]));
+
+/**
+ * eliminateTeam(teams):
+ * - receives array `teams`
+ * - removes the last element from the array and return it
+ *
+ * e.g.
+ * eliminateTeam(["Brazil", "Germany", "Italy"]) -> "Italy"
+ */
+function eliminateTeam(teams) {
+  return teams.pop();
+}
+console.log(eliminateTeam(["Brazil", "Germany", "Italy"]));
